e4908ca0f6ce1f6e4a6fdb3347771767850fc9f4
unknown <zloymax31@gmail.com>
2023-08-13 16:25:32 +0300

    finished

--- index.js
+++ index.js
@@ -21,6 +21,39 @@ console.log(totalSum);
 
 const numbersss = [5, 10 ,15 ,20, 25];
 
-for (i = 0; i = numbersss.length; i++) {
+for (i = 0; i < numbersss.length; i++) {
     console.log(numbersss[i]);
 }
+
+const stringss = [
+"Ko", 
+"nygu", 
+"Jolul", 
+"Firuss", 
+"Nobejos"
+];
+
+for (i = 0; i < stringss.length; i++) {
+    if (stringss[i].length > 5) {
+        console.log(stringss[i]);
+    }
+}
+
+const numberssss = [1, 2, 3, 6, 100, 101, 55, 1000, 1111, 2000];
+let maxNumber = numberssss[0];
+
+for (i = 1; i < numberssss.length; i++) {
+    if (numberssss[i] > maxNumber) {
+        maxNumber = numberssss[i];
+    }
+}
+console.log("Найвище число в масиві =>", maxNumber);
+
+const numbersssss = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = [];
+for (i = 0; i < numbersssss.length; i++) {
+    if (numbersssss[i] % 2 === 0) {
+      evenNumbers.push(numbersssss[i]);
+    }
+  }
+console.log("Парні числа =>", evenNumbers);
